7065bff4cf872efba954fc782dede9a03257771e
aaronzamador <70031635+aaronzamador@users.noreply.github.com>
2021-03-29 20:04:20 -0500

    clean up

--- js/index3.js
+++ js/index3.js
@@ -43,3 +43,20 @@ $(window).scroll(function() {
 
 
 
+$("#natilation").click(function(){
+
+
+    if ($('.rotatingm').hasClass("cmen")){
+        $(".rotatingm").removeClass("cmen");
+        $('.sside').removeClass("cside");
+        $('body').removeClass("pagesize");
+        $('.hex').removeClass("sideclick");
+    }
+    else{
+        $('.rotatingm').addClass("cmen");
+        $('.sside').addClass("cside");
+        $('body').addClass("pagesize");
+        $('.hex').addClass("sideclick");}
+
+
+});
